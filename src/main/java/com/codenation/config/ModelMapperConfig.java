ba0ff9fb8bb710f4304a0755db79eb8779a00c75
Lucas <luc.zago@hotmail.com>
2021-04-20 09:18:59 -0300

    Corrigindo ModelMapper

--- src/main/java/com/codenation/config/ModelMapperConfig.java
+++ src/main/java/com/codenation/config/ModelMapperConfig.java
@@ -1,20 +1,11 @@
 package com.codenation.config;
 
-import com.codenation.dtos.UserDTO;
-import com.codenation.models.User;
-import lombok.AllArgsConstructor;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
-
-    @AllArgsConstructor
-    private class Fullname {
-        private String name;
-    }
 
     @Bean
     public ModelMapper modelMapper() {
